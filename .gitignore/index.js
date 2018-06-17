6866906387fa46d3d2393aff3bcd8d0346e45bd1
crazybahna <40315064+crazybahna@users.noreply.github.com>
2018-06-17 14:01:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client(); 
 
-bot.login('NDU2MTM0Mzc1MTI2NDY2NTcw.DgGfTg.vRPpghEw02PQQWhzHy60KqP3Oeo');
+bot.login(process.env.BOT_TOKEN);
 
 bot.on('message', function (message){
 	if(message.content ==='!ibahna'){
